0cfba629e4c8d40a8851bf5e989a87d2fb4a455c
Ryan Lv <xiaorong.ruby@gmail.com>
2020-11-30 14:08:31 +0800

    test file placehold

--- lib/formi9_compliance/oauth.rb
+++ lib/formi9_compliance/oauth.rb
@@ -1,11 +1,11 @@
 module Formi9Compliance
   module OAuth
-    def token_cache_key
+    def access_token_cache_key
       Digest::MD5.hexdigest([partner_id, username, password].join)
     end
 
     def access_token
-      token = Rails.cache.read(cache_key)
+      token = Rails.cache.read(access_token_cache_key)
 
       if token.present?
         return token

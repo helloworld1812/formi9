4080dcefdfe8b0af7d62574a990f5c9e03ef365c
snow <snow.helsing@gmail.com>
2022-06-22 23:18:21 +0800

    replace obsoleted `URI.encode` with `Addressable::URI.escape`

--- lib/formi9/request.rb
+++ lib/formi9/request.rb
@@ -22,14 +22,14 @@ module Formi9
     def delete(path, options={})
       request(:delete, path, options)
     end
-    
+
     def request(method, path, options)
       response = connection.send(method) do |request|
         case method
         when :get, :delete
-          request.url(URI.encode(path), options)
+          request.url(Addressable::URI.escape(path), options)
         when :post, :put
-          request.path = URI.encode(path)
+          request.path = Addressable::URI.escape(path)
           request.body = options
         end
       end
